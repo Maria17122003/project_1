847c0061013e6eacd7045fe615e2c6d72bdf82f8
Maria <mararyabina62@gmail.com>
2023-01-27 23:05:52 +1000

    добавили функции, которые меняют регистр в строке

--- str_func.py
+++ str_func.py
@@ -5,3 +5,11 @@ def capital_letters(letters):
     """
     return letters.upper()
 
+
+def first_letter_capital(letters):
+    """
+    Возвращает строку
+    с первой заглавной буквой
+    каждого слова
+    """
+    return letters.title()
