4dc6df16908e306548c33b0bf40396977e1d8e48
Maria17122003 <123393865+Maria17122003@users.noreply.github.com>
2023-01-27 22:40:23 +1000

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -5,12 +5,4 @@ def capital_letters(letters):
     """
     return letters.upper()
 
-def first_letter_capital(letters):
-    """
-    Возвращает строку
-    с первой заглавной буквой
-    каждого слова
-    """
-    return letters.title()
-
 
